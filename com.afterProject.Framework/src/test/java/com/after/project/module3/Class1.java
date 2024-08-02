b0f49a838523dcedcfca781d74730f4da2b9fa3d
DeepakTheGreat <DeepakTheGreat@DeepakLappy>
2024-08-02 12:50:52 +0530

    2.0

---
+++ com.afterProject.Framework/src/test/java/com/after/project/module3/Class1.java
@@ -0,0 +1,15 @@
+package com.after.project.module3;
+
+public class Class1 
+{
+public static void main(String[] args)
+{
+	System.out.println("Divya");
+	
+	String name;
+
+	String lastname;
+
+	String address;
+}
+}
